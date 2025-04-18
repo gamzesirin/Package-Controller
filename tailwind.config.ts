9e43a1ad7b6232d856afbeab4dc4c989aac349a1
Gamze Şirin <gamze1sirin@gmail.com>
2025-04-18 15:43:40 +0300

    feat (package.json ): add@radix-ui/react-switch, i18next, i18next-browser-languagedetector, i18next-resources-to-backend, next-themes ve react-i18next.

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -10,6 +10,10 @@ const config: Config = {
 	],
 	theme: {
 		extend: {
+			fontFamily: {
+				lexend: ['var(--font-lexend)'],
+				poppins: ['var(--font-poppins)']
+			},
 			backgroundImage: {
 				'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
 				'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))'
